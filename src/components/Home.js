e99a9f4de1798b6113b1f07d1829914a497e8c5d
AdriRdz <adirodriguez1015@gmail.com>
2022-12-12 22:35:08 -0400

    Adicionando o footer

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,78 @@
-import arvore2 from "../assets/arvore2.jpg"
-import "./Index.css"
+import livro2 from "../assets/livro2.jpeg";
+import "./Index.css";
+import { Link } from 'react-router-dom';
+import { FaFacebook, FaInstagram, FaLinkedin, FaEnvelope, FaPhoneAlt } from 'react-icons/fa';
 
 
 
 
 const Home = () => {
     return (
-        <div className="body" style={{ backgroundImage: `url(${arvore2})` }}>
 
-            <div>
-                <h1>Sistema de Cadastro de Alunos</h1>
+        <div>
+            <div className="body" style={{ backgroundImage: `url(${livro2})` }}>
+
+                <div>
+                    <h1 className="cadastro">Instituto de Tecnologia e Saber</h1>
+                </div>
+                <div>
+                    <p className="titulo">Sistema de Cadastro de Alunos</p>
+                </div>
+                <div>
+                    <p className="slogan">"Um programador que escreve código limpo, entende totalmente o problema antes de escrever o código."</p>
+                    </div>
             </div>
-            <div>
-                <p className="titulo">Instituto de Tecnologia e Saber</p>
-            </div>
-            <div>
-                <p className="slogan">"Um programador que escreve código limpo, entende totalmente o problema antes de escrever o código."</p>
-            </div>
+            <div className='container'>
+                <div className='row'>
+                    <div className='footer-col'>
+                        <h4>Instituto de Tecnologia e Saber</h4>
+                        <ul>
+                            <li><Link href=''>Desenvolvido por:</Link></li>
+                            <li><Link href='/'>Adriana Rodriguez</Link></li>
+                            <li><Link href='/'>Jose Lumbo</Link></li>
+                            <li><Link href='/'>Nelson Villarroel</Link></li>
+                            <li><Link href='/'>Walid Abdelfattah</Link></li>
+                        </ul>
+                    </div>
+                    <div className='footer-col'>
+                        <h4>Obter ajuda</h4>
+                        <ul>
+                            <li><Link href='/'>Perguntas Frequentes</Link></li>
+                            <li><Link href='/'>Transporte</Link></li>
+                             <li><Link href='/'>Status de Alunos</Link></li>
+                            <li><Link href='/'>Opção de pagamento</Link></li>
+                        </ul>
+                    </div>
+
+                    <div className='footer-col'>
+                        <h4>Produtos mais vendidos</h4>
+                        <ul>
+                            <li><a href='#'>Ração</a></li>
+                            <li><a href='#'>Cama</a></li>
+                            <li><a href='#'>Leite</a></li>
+                            <li><a href='#'>Outros</a></li>
+                        </ul>
+
+                    </div>
+
+                    <div className='footer-col'>
+                        <p>Siga-nos</p>
+                        <div className='social-links'>
+                            <ul>
+                                <li><Link><FaFacebook /></Link></li>
+                                <li><Link> <FaInstagram /></Link></li>
+                                <li><Link> <FaLinkedin /></Link></li>
+                                <li><FaEnvelope /> tecnosaber@gmail.com</li>
+                                <li><FaPhoneAlt />(55)55555-5555</li>
+                                <li>Instuto de Tecnologia e Saber &copy; 2022</li>
+                            </ul>
+                        </div>
+                    </div>
+                        </div>
+                            </div>
+                            
         </div>
+
     )
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import { AppBar, Toolbar, styled } from '@mui/material';
 import { NavLink } from 'react-router-dom';
 
 const Header = styled(AppBar)`
-background-image: linear-gradient(135deg, #8EB5F0, #290a52, #300752, #1c1c6e, #1b2c4e, #1f1d86, #3c29e4, #092164);  
+background-image: linear-gradient(135deg, #171718, #2b2b2c, #323233, #464647, #5d5d5e, #6c6a6d, #716e74, #87858a);
 `
 
 const Tabs = styled(NavLink)`
@@ -12,7 +12,8 @@ margin-right: 20px;
 color: white;
 text-decoration: none;
 font-weight: bold;
-justify-content: end;
+float: right;
+text-align: center;
 `
 
 const NavBar = () => {
@@ -20,10 +21,9 @@ const NavBar = () => {
         <Header position='static'>
             <Toolbar>
                 
-                <Tabs to="/">Instituto de tecnologia e saber</Tabs>
                 <Tabs to="/">Home</Tabs>
-                <Tabs to="/all">Todos os alunos</Tabs>
-                <Tabs to="/add">Cadastrar Alunos</Tabs>
+                <Tabs to="/all">Alunos</Tabs>
+                <Tabs to="/add">Cadastrar</Tabs>
                 
             </Toolbar>
         </Header>
